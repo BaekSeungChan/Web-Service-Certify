5f06185bc6631685a713a4ae7c5da0f5befdc087
baekseungchan1117 <bsc7386@gmail.com>
2024-01-17 19:35:31 +0900

    JWT Token 인증

--- src/test/java/com/example/webservicecertify/WebServiceCertifyApplicationTests.java
+++ src/test/java/com/example/webservicecertify/WebServiceCertifyApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.webservicecertify;
 
+import com.example.webservicecertify.jwt.service.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
 
 @SpringBootTest
 class WebServiceCertifyApplicationTests {
 
+    @Autowired
+    private JwtService jwtService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void tokenCreate(){
+        var claims = new HashMap<String, Object>();
+        claims.put("user_id", 923);
+
+        var expireAt = LocalDateTime.now().plusMinutes(10);
+
+        var jwtToken = jwtService.create(claims, expireAt);
+
+        System.out.println(jwtToken);
+    }
+
+    @Test
+    void tokenValidation(){
+        var token = " eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5MjMsImV4cCI6MTcwNTQ4NzU0NX0.GgPXatb8zaAvpcCiBUrVXVaFXJ_eSW2wE8L7CIHnKAw";
+        jwtService.validation(token);
     }
 
 }
