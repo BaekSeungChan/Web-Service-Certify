ce3813464d210b50be05f7adcb48589894285072
baekseungchan1117 <bsc7386@gmail.com>
2024-01-17 12:47:48 +0900

    init

---
+++ src/main/java/com/example/webservicecertify/WebServiceCertifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicecertify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceCertifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceCertifyApplication.class, args);
+    }
+
+}
